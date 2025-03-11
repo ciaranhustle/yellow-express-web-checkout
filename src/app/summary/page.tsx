f2ec62cbce789168a48f9a63e3b4467fdff1099e
Dimitri <dimitri.tsardakas@gmail.com>
2025-03-11 18:34:41 +1100

    only show DIY/TLC and assist acknowledgmenet for Big & Bulky items

--- src/app/summary/page.tsx
+++ src/app/summary/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Container } from "@/components/Container/Container";
 import { Loader } from "@/components/Loader/Loader";
 import { BookingSummary } from "@/components/BookingSummary/BookingSummary";
@@ -26,6 +26,18 @@ const SummaryPage = () => {
   const [isServiceAcknowledge, setIsServiceAcknowledge] = useState(false);
   const [isChangeAcknowledge, setIsChangeAcknowledge] = useState(false);
 
+  const handleSetBookingAssistOption = useCallback((option: BookingAssistOption) => {
+    setSelectedAssistOption(option);
+    dispatch({ type: "SET_BOOKING_ASSIST_OPTION", payload: option });
+  }, [dispatch]);
+
+  // Set DIY by default for small items
+  useEffect(() => {
+    if (quote && quote.bookingDetails.bookingType === 'Small Items' && !selectedAssistOption) {
+      handleSetBookingAssistOption('DIY');
+    }
+  }, [quote, handleSetBookingAssistOption, selectedAssistOption]);
+
   const handlePaymentSuccess = async (paymentMethodId: string) => {
     if (!quote || !selectedAssistOption) return;
     checkout({ paymentMethodId, quote, bookingAssistOption: selectedAssistOption});
@@ -33,11 +45,6 @@ const SummaryPage = () => {
 
   const handlePaymentError = () => {
     toast.error('Payment failed. Please try again.');
-  };
-
-  const handleSetBookingAssistOption = (option: BookingAssistOption) => {
-    setSelectedAssistOption(option);
-    dispatch({ type: "SET_BOOKING_ASSIST_OPTION", payload: option });
   };
 
   useEffect(() => {
@@ -66,57 +73,67 @@ const SummaryPage = () => {
                 </p>
                 <BookingSummary quote={quote} />
 
-                <p className="text-2xl font-bold my-8">Select an option</p>
-                <div className="flex flex-row gap-5">
-                  <BookingAssistOption
-                    option="DIY"
-                    description="We'll park curbside and help you load & unload our van"
-                    selectedOption={selectedAssistOption}
-                    setSelectedOption={handleSetBookingAssistOption}
-                    fullPrice={quote.fullPrice}
-                    price={quote.price}
-                    hint="Best value"
-                  />
-                  <BookingAssistOption
-                    option="TLC"
-                    description="Sit back and relax, we'll do all the work."
-                    selectedOption={selectedAssistOption}
-                    setSelectedOption={handleSetBookingAssistOption}
-                    fullPrice={quote.tlcFullPrice}
-                    price={quote.tlcPrice}
-                  />
-                </div>
-
-                {selectedAssistOption && (
-                  <div className="px-2">
-                    <p className="text-xl font-bold mt-7 mb-2 text-start">
-                      Acknowledge before proceeding
-                    </p>
-                    <div className="w-full flex flex-col gap-5">
-                      <Acknowledgement
-                        text={
-                          selectedAssistOption === "DIY"
-                            ? "I understand that Yellow Express DIY provides a one-man and van service. I agree to assist the driver with loading and unloading any large or bulky items at both pickup and drop-off locations."
-                            : "I understand that Yellow Express TLC provides a two-man and van service. I agree to ensure that the drivers have access in order to load and unload any large or bulky items at both pickup and drop-off locations."
-                        }
-                        isChecked={isServiceAcknowledge}
-                        onChange={() =>
-                          setIsServiceAcknowledge((prev) => !prev)
-                        }
+                {quote.bookingDetails.bookingType === 'Big & Bulky' && (
+                  <>
+                    <p className="text-2xl font-bold my-8">Select an option</p>
+                    <div className="flex flex-row gap-5">
+                      <BookingAssistOption
+                        option="DIY"
+                        description="We'll park curbside and help you load & unload our van"
+                        selectedOption={selectedAssistOption}
+                        setSelectedOption={handleSetBookingAssistOption}
+                        fullPrice={quote.fullPrice}
+                        price={quote.price}
+                        hint="Best value"
                       />
-                      <Acknowledgement
-                        text="I understand that any changes to the job details (such as item quantity, size, location, or time) may result in a change to the final price."
-                        isChecked={isChangeAcknowledge}
-                        onChange={() => setIsChangeAcknowledge((prev) => !prev)}
+                      <BookingAssistOption
+                        option="TLC"
+                        description="Sit back and relax, we'll do all the work."
+                        selectedOption={selectedAssistOption}
+                        setSelectedOption={handleSetBookingAssistOption}
+                        fullPrice={quote.tlcFullPrice}
+                        price={quote.tlcPrice}
                       />
                     </div>
-                  </div>
+
+                    <div className="px-2">
+                      <p className="text-xl font-bold mt-7 mb-2 text-start">
+                        Acknowledge before proceeding
+                      </p>
+                      <div className="w-full flex flex-col gap-5">
+                        <Acknowledgement
+                          text={
+                            selectedAssistOption === "DIY"
+                              ? "I understand that Yellow Express DIY provides a one-man and van service. I agree to assist the driver with loading and unloading any large or bulky items at both pickup and drop-off locations."
+                              : "I understand that Yellow Express TLC provides a two-man and van service. I agree to ensure that the drivers have access in order to load and unload any large or bulky items at both pickup and drop-off locations."
+                          }
+                          isChecked={isServiceAcknowledge}
+                          onChange={() =>
+                            setIsServiceAcknowledge((prev) => !prev)
+                          }
+                        />
+                      </div>
+                    </div>
+                  </>
                 )}
+
+                <div className="px-2 my-6">
+                  <div className="w-full flex flex-col gap-5">
+                    <Acknowledgement
+                      text="I understand that any changes to the job details (such as item quantity, size, location, or time) may result in a change to the final price."
+                      isChecked={isChangeAcknowledge}
+                      onChange={() => setIsChangeAcknowledge((prev) => !prev)}
+                    />
+                  </div>
+                </div>
 
                 <div
                   className={cn(
                     "w-full flex flex-col",
-                    (!selectedAssistOption || !isServiceAcknowledge || !isChangeAcknowledge) && "blur-sm pointer-events-none"
+                    ((quote.bookingDetails.bookingType === 'Big & Bulky' && 
+                      (!selectedAssistOption || !isServiceAcknowledge)) || 
+                     !isChangeAcknowledge) && 
+                    "blur-sm pointer-events-none"
                   )}
                 >
                   <div className="w-full bg-white border-b-8 border-b-primary rounded shadow-lg pl-6 pr-2 pt-4 pb-2 text-black my-7 flex flex-row justify-between items-center">
