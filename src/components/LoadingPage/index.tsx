23173d7241efbd17d5420d658847db0ac0687e82
ciaranhustle <ciaran@hustle.agency>
2025-04-17 13:18:18 +1000

    More styling work to stop double scroll

--- src/components/LoadingPage/index.tsx
+++ src/components/LoadingPage/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Loader } from '../Loader/Loader';
+import React from "react";
+import { Loader } from "../Loader/Loader";
 
 interface LoadingPageProps {
   message?: string;
@@ -11,7 +11,7 @@ export const LoadingPage: React.FC<LoadingPageProps> = ({
   description,
 }) => {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="h-full flex items-center justify-center">
       <div className="max-w-lg w-full py-10 px-4">
         <div className="flex flex-col items-center space-y-6">
           <Loader />
@@ -19,12 +19,10 @@ export const LoadingPage: React.FC<LoadingPageProps> = ({
             {message}
           </h2>
           {description && (
-            <p className="text-gray-500 text-center">
-              {description}
-            </p>
+            <p className="text-gray-500 text-center">{description}</p>
           )}
         </div>
       </div>
     </div>
   );
-}; 
+};
